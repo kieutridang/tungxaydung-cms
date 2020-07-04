3cf809a43bad6d41f26f7a952fa7db55fe72f361
Kiều Trí Đăng <kieutridang@gmail.com>
2020-07-05 02:08:11 +0700

    remove bug

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -138,30 +138,3 @@ const Footer = class extends React.Component {
 };
 
 export default Footer;
-
-<!-- Load Facebook SDK for JavaScript -->
-      <div id="fb-root"></div>
-      <script>
-        window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v7.0'
-          });
-        };
-
-        (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
-
-      <!-- Your Chat Plugin code -->
-      <div class="fb-customerchat"
-        attribution=setup_tool
-        page_id="110184210682682"
-  theme_color="#FF4400"
-  logged_in_greeting="Xin chào quý khách. Em có thể giúp gì cho anh/chị ạ?"
-  logged_out_greeting="Xin chào quý khách. Em có thể giúp gì cho anh/chị ạ?">
-      </div>
